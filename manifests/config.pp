d27b612cc727c65680b54e09cec95d0ddf5c0114
Nicolas Corrarello <ncorrare@gmail.com>
2015-02-03 16:41:29 +0000

    addauthyubico is now defined outside a class

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,26 +6,20 @@ class yubikey::config (
   $beforemod=$yubikey::params::beforemod,
 ) {
   require ::yubikey::install
-  # Define a type to iterate through the $service array
-  define addAuthYubico {
-    pam { "Insert Yubikey entry on ${name} as ${yubikey::config::control}":
-      ensure    => present,
-      service   => $name,
-      type      => 'auth',
-      control   => $yubikey::config::control,
-      module    => 'pam_yubico.so',
-      arguments => $yubikey::config::arguments,
-      position  => "before *[type=\"auth\" and module=\"${yubikey::config::beforemod}\"]",
-      }
-      }
-  if $::kernel =='Linux' and ($::osfamily == 'RedHat' or $::osfamily == 'Debian') {
+  if $::kernel =='Linux' and
+  ($::osfamily == 'RedHat' or $::osfamily == 'Debian') {
     if 'debug' in $arguments {
       file { '/var/run/pam-debug.log' :
         ensure => present,
         mode   => '0777'
       }
     }
-    addAuthYubico { $service: }
+    yubikey::addauthyubico { $service :
+    arguments => $arguments,
+    control   => $control,
+    beforemod => $beforemod,
+    }
+
   } else {
     notice ("${::operatingsystem} is not supported")
   }
