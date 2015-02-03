6faeec8ef59d8c22f7ae7421796a6956089fd919
Nicolas Corrarello <ncorrare@gmail.com>
2015-02-03 08:30:49 +0000

    More linting

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,8 +1,9 @@
+#Defining the type yubikey:key to allow users to authenticate with Yubikeys.
 define yubikey::key ($token) {
   require ::yubikey::config
-  file { "/home/$name/.yubico/authorized_yubikeys" :
-    ensure => 'present',
-    content => "$name:$token",
+  file { "/home/${name}/.yubico/authorized_yubikeys" :
+    ensure  => 'present',
+    content => "${name}:${token}",
   }
 }
 
