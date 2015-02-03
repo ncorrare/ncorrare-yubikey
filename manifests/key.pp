ce51b9422cbbb83b1909e9061513f56df76ead40
Nicolas Corrarello <ncorrare@gmail.com>
2015-02-03 13:34:18 +0000

    Ask for the group and homedir to create the file in the right place, with the right permisions

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,9 +1,17 @@
 #Defining the type yubikey:key to allow users to authenticate with Yubikeys.
-define yubikey::key ($token) {
+define yubikey::key ($token,$home,$group) {
   require ::yubikey::config
-  file { "/home/${name}/.yubico/authorized_yubikeys" :
+  file { "${home}/.yubico/" :
+    ensure => directory
+    owner  => $name, 
+    group  => $group,
+  }
+  file { "${home}/.yubico/authorized_yubikeys" :
     ensure  => 'present',
+    owner   => $name,
+    group   => $group,
     content => "${name}:${token}",
+    require => File["${home}/.yubico/"]
   }
 }
 
