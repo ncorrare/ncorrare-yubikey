a198aed62014e6869d7c2983578fb4f8ba97004b
Nicolas Corrarello <ncorrare@gmail.com>
2015-02-03 14:40:25 +0000

    Defined the right scope now that I need a type to iterate the PAM services

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,29 +3,29 @@ class yubikey::config (
   $arguments=$yubikey::params::arguments,
   $service=$yubikey::params::service,
   $control=$yubikey::params::control,
-  $beforemod=$yubikey::params::beforemod
+  $beforemod=$yubikey::params::beforemod,
 ) {
   require ::yubikey::install
   define addAuthYubico {
-    pam { "Insert Yubikey entry on $name":
+    pam { "Insert Yubikey entry on $name as $yubikey::config::control":
       ensure    => present,
       service   => $name,
       type      => 'auth',
-      control   => $control,
+      control   => $yubikey::config::control,
       module    => 'pam_yubico.so',
-      arguments => $arguments,
-      position  => "before *[type=\"auth\" and module=\"${beforemod}\"]",
+      arguments => $yubikey::config::arguments,
+      position  => "before *[type=\"auth\" and module=\"${yubikey::config::beforemod}\"]",
       }
       #If debug is enabled, create a debug file"
    }
-   if $::kernel =='Linux'  {
+   if $::kernel =='Linux' and ($::osfamily == 'RedHat' or $::osfamily == 'Debian') {
      if 'debug' in $arguments {
        file { "/var/run/pam-debug.log" :
         ensure => present,
         mode => '0777'
       }
     }
-    addAuthYubico { $service: }     
+    addAuthYubico { $service: } 
   } else {
     notice ("${::operatingsystem} is not supported")
   }
